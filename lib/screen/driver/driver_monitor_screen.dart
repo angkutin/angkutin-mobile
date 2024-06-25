3d36e2b623c40317a9c0ceb85904b38653363248
famuh <famuh79@gmail.com>
2024-06-25 15:15:06 +0700

    FEATURE : driver can display route using openrouteservice

--- lib/screen/driver/driver_monitor_screen.dart
+++ lib/screen/driver/driver_monitor_screen.dart
@@ -1,20 +1,21 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:async';
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'package:location/location.dart';
+import 'package:provider/provider.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 import 'package:angkutin/common/state_enum.dart';
 import 'package:angkutin/screen/driver/driver_gome_screen.dart';
 import 'package:angkutin/screen/driver/service/DriverLocationService.dart';
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:provider/provider.dart';
-
 import 'package:angkutin/common/utils.dart';
 import 'package:angkutin/provider/monitor_provider.dart';
 import 'package:angkutin/widget/CustomListTile.dart';
-
 import '../../data/model/RequestModel.dart';
 import '../../provider/driver/driver_service_provider.dart';
 import '../../widget/CustomButton.dart';
@@ -38,17 +39,15 @@ class DriverMonitorScreen extends StatefulWidget {
 
 class _DriverMonitorScreenState extends State<DriverMonitorScreen> {
   GoogleMapController? _mapController;
-  // LatLng? _userLocation;
   LatLng? _driverLocation;
   LatLng? _previousDriverLocation;
   Set<Marker> markers = {};
+  Set<Polyline> polylines = {};
   Timer? _dataTimer;
-      StreamSubscription<GeoPoint>? locationSubscription;
-
+  StreamSubscription<GeoPoint>? locationSubscription;
 
   double latitude = 0;
   double longitude = 0;
-
   GeoPoint? userLocationLatLng;
 
   @override
@@ -63,12 +62,10 @@ class _DriverMonitorScreenState extends State<DriverMonitorScreen> {
   void initState() {
     super.initState();
 
-    // Initialize the marker with the user's initial location
     markers.add(
       Marker(
         markerId: const MarkerId('userMarker'),
-        position:
-            LatLng(widget.userLocation.latitude, widget.userLocation.longitude),
+        position: LatLng(widget.userLocation.latitude, widget.userLocation.longitude),
         infoWindow: const InfoWindow(title: 'Lokasi Permintaan'),
       ),
     );
@@ -87,8 +84,6 @@ class _DriverMonitorScreenState extends State<DriverMonitorScreen> {
 
   _loadData() async {
     _loadAndUpdateDriverLocation();
-
-// get data spesifik
     Provider.of<MonitorProvider>(context, listen: false)
         .getRequestDataStream(widget.type, widget.requestId);
   }
@@ -103,23 +98,19 @@ class _DriverMonitorScreenState extends State<DriverMonitorScreen> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       setState(() {
         _driverLocation = newLocation;
-        markers.removeWhere(
-            (marker) => marker.markerId == const MarkerId('driverMarker'));
+        markers.removeWhere((marker) => marker.markerId == const MarkerId('driverMarker'));
         markers.add(
           Marker(
             markerId: const MarkerId('driverMarker'),
             position: _driverLocation!,
-            icon: BitmapDescriptor.defaultMarkerWithHue(
-                BitmapDescriptor.hueGreen),
+            icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
             infoWindow: const InfoWindow(title: 'Lokasi Petugas'),
           ),
         );
 
-        _mapController?.animateCamera(
-          CameraUpdate.newLatLng(_driverLocation!),
-        );
+        _mapController?.animateCamera(CameraUpdate.newLatLng(_driverLocation!));
 
-        print("Driver location updated to: $_driverLocation");
+        _fetchRoute(LatLng(widget.userLocation.latitude, widget.userLocation.longitude), _driverLocation!);
       });
     });
   }
@@ -127,27 +118,47 @@ class _DriverMonitorScreenState extends State<DriverMonitorScreen> {
   _loadAndUpdateDriverLocation() async {
     LocationService locationService = LocationService();
 
-    locationSubscription =
-        locationService.locationStream.listen((userLocation) {
+    locationSubscription = locationService.locationStream.listen((userLocation) {
       setState(() {
         latitude = userLocation.latitude;
         longitude = userLocation.longitude;
 
         userLocationLatLng = GeoPoint(latitude, longitude);
-
-        // _updateDriverLocationOnServer(
-        //     "Bw2aq2Q0OYglg35x0Kfi", userLocationLatLng!);
       });
     });
   }
 
-  // _updateDriverLocationOnServer(String reqId, GeoPoint driverLoc) async {
-  //   final driverServiceProv =
-  //       Provider.of<DriverServiceProvider>(context, listen: false);
-  //   await driverServiceProv.updateDriverLocation(reqId, driverLoc);
+  Future<void> _fetchRoute(LatLng origin, LatLng destination) async {
+    final String apiKey = dotenv.env['OPENROUTESERVICE_API_KEY']!;
+    final String url =
+        'https://api.openrouteservice.org/v2/directions/driving-car?api_key=$apiKey&start=${origin.longitude},${origin.latitude}&end=${destination.longitude},${destination.latitude}';
 
-  //   // print("Lokasi diupdate pada __updateDriverLocation");
-  // }
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final coordinates = data['features'][0]['geometry']['coordinates'];
+      _createPolylines(coordinates);
+    } else {
+      print('Failed to fetch directions: ${response.body}');
+    }
+  }
+
+  void _createPolylines(List<dynamic> coordinates) {
+    List<LatLng> polylineCoordinates = [];
+    for (var coordinate in coordinates) {
+      polylineCoordinates.add(LatLng(coordinate[1], coordinate[0]));
+    }
+
+    setState(() {
+      polylines.clear();
+      polylines.add(Polyline(
+        width: 5,
+        polylineId: PolylineId("poly"),
+        color: Colors.blue,
+        points: polylineCoordinates,
+      ));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -178,14 +189,14 @@ class _DriverMonitorScreenState extends State<DriverMonitorScreen> {
                       child: GoogleMap(
                         initialCameraPosition: CameraPosition(
                           target: _driverLocation ??
-                              const LatLng(3.5649873243206964,
-                                  98.71563527362277), // initial data
+                              const LatLng(3.5649873243206964, 98.71563527362277), // initial data
                           zoom: 15,
                         ),
                         onMapCreated: (GoogleMapController controller) {
                           _mapController = controller;
                         },
                         markers: markers,
+                        polylines: polylines,
                       )),
                   Row(
                     children: [
@@ -206,11 +217,8 @@ class _DriverMonitorScreenState extends State<DriverMonitorScreen> {
                                         request.idPetugas!);
 
                                     // balik ke home
-                                    if (driverServiceProv.finishState ==
-                                        ResultState.success) {
-                                      Future.delayed(
-                                          const Duration(milliseconds: 500),
-                                          () {
+                                    if (driverServiceProv.finishState == ResultState.success) {
+                                      Future.delayed(const Duration(milliseconds: 500), () {
                                         Navigator.pushReplacementNamed(
                                           context,
                                           DriverHomeScreen.ROUTE_NAME
@@ -230,28 +238,20 @@ class _DriverMonitorScreenState extends State<DriverMonitorScreen> {
                       title: "Diajukan oleh", value: "An. ${request.name}"),
                   CustomListTile(
                       title: "Waktu Permintaan",
-                      value:
-                          "${formatDate(request.date.toDate().toString())}, ${formatTime(request.date.toDate().toString())}"),
+                      value: "${formatDate(request.date.toDate().toString())}, ${formatTime(request.date.toDate().toString())}"),
                   CustomListTile(
                       title: "Deskripsi",
-                      value: request.description! != ''
-                          ? request.description!
-                          : "-"),
+                      value: request.description! != '' ? request.description! : "-"),
                   Container(
                     width: mediaQueryWidth(context),
                     height: 200,
                     padding: const EdgeInsets.all(16),
-                    // decoration: containerBorderWithRadius.copyWith(
-                    //     border: Border.all(color: softBlueColor)),
                     child: CachedNetworkImage(
                       imageUrl: request.imageUrl,
                       fit: BoxFit.cover,
-                      progressIndicatorBuilder:
-                          (context, url, downloadProgress) =>
-                              CircularProgressIndicator(
-                                  value: downloadProgress.progress),
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error),
+                      progressIndicatorBuilder: (context, url, downloadProgress) =>
+                          CircularProgressIndicator(value: downloadProgress.progress),
+                      errorWidget: (context, url, error) => const Icon(Icons.error),
                     ),
                   ),
                 ],
